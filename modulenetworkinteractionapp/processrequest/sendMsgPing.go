1180ab7773b0fa759087eaebec81230ae17e436f
Artemiy <beljakovav@gmail.com>
2019-04-04 17:55:37 +0300

    ISEMS-NIH_master 04.04.2019

---
+++ modulenetworkinteractionapp/processrequest/sendMsgPing.go
@@ -0,0 +1,29 @@
+package processrequest
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"ISEMS-NIH_master/configure"
+)
+
+//SendMsgPing отправить сообщение типа Ping
+func SendMsgPing(ss *configure.SourceSetting) ([]byte, error) {
+	fmt.Println("для клиента, отправить запрос PING")
+
+	msgPing := configure.MsgTypePingPong{
+		MsgType: "ping",
+		Info: configure.DetailInfoMsgPingPong{
+			MaxCountProcessFiltration: ss.Settings.MaxCountProcessFiltration,
+			EnableTelemetry:           ss.Settings.EnableTelemetry,
+			StorageFolders:            ss.Settings.StorageFolders,
+		},
+	}
+
+	formatJSON, err := json.Marshal(msgPing)
+	if err != nil {
+		return nil, err
+	}
+
+	return formatJSON, nil
+}
