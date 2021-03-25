71e506fe2a70dd52a709b50be725f3718f3aba02
Artemij Belyakov <beljakovav@gmail.com>
2021-03-25 14:43:49 +0300

    ISEMS-NIH_master 25.03.2021

--- mytestpackages/packetDecoder_test.go
+++ mytestpackages/packetDecoder_test.go
@@ -18,294 +18,78 @@ import (
 )
 
 var _ = Describe("PacketDecoder", func() {
-	var (
-		file            *os.File
-		errOpenFile     error
-		fd              *os.File
-		errFd           error
-		handleOnlyIP    *pcap.Handle
-		errOnlyIP       error
-		errBPFIP        error
-		wfOnlyIP        *os.File
-		errwfOnlyIP     error
-		nwfOnlyIP       *pcapgo.Writer
-		handleOnlyPPPoE *pcap.Handle
-		errOnlyPPPoE    error
-		errBPFPPPoE     error
-		wfOnlyPPPoE     *os.File
-		errwfOnlyPPPoE  error
-		nwfOnlyPPPoE    *pcapgo.Writer
-	)
-
-	fileNameOnlyIP := "1616398942_2021_03_22____10_42_22_21.tdp"
-	filePathOnlyIP := "/Users/user/pcap_test_files/ip"
-
-	fileNameOnlyPPPoE := "1616149545_2021_03_19____13_25_45_3596.tdp"
-	filePathOnlyPPPoE := "/Users/user/pcap_test_files/pppoe"
-
 	var _ = BeforeSuite(func() {
 		//удаляем файлы результатов обработки
 		func() {
 			if err := os.Remove("/Users/user/pcap_test_files/pcapinfoFileOnlyIP.txt"); err != nil {
-				fmt.Println(err)
+				fmt.Printf("ERROR: %v\n", err)
 			}
 			if err := os.Remove("/Users/user/pcap_test_files/pcapinfoFileOnlyIP.pcap"); err != nil {
-				fmt.Println(err)
+				fmt.Printf("ERROR: %v\n", err)
 			}
 			if err := os.Remove("/Users/user/pcap_test_files/pcapinfoFileOnlyPPPoE.pcap"); err != nil {
-				fmt.Println(err)
+				fmt.Printf("ERROR: %v\n", err)
 			}
 		}()
-
-		/* для файла по которому выполняется декодирование пакетов */
-		file, errOpenFile = os.Open(path.Join(filePathOnlyIP, fileNameOnlyIP))
-
-		/* для файла в который выполняется запись информации полученной в результате декодирования */
-		fd, errFd = os.OpenFile("/Users/user/pcap_test_files/pcapinfoFileOnlyIP.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-
-		/* для сет. трафика содержащего только IP */
-		handleOnlyIP, errOnlyIP = pcap.OpenOffline(path.Join(filePathOnlyIP, fileNameOnlyIP))
-		errBPFIP = handleOnlyIP.SetBPFFilter("tcp && host 77.241.31.37")
-
-		wfOnlyIP, errwfOnlyIP = os.OpenFile("/Users/user/pcap_test_files/pcapinfoFileOnlyIP.pcap", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-		nwfOnlyIP = pcapgo.NewWriter(wfOnlyIP)
-		if err := nwfOnlyIP.WriteFileHeader(1600, layers.LinkTypeEthernet); err != nil {
-			fmt.Println(err)
-		}
-
-		/* для сет. трафика содержащего только PPPoE */
-		handleOnlyPPPoE, errOnlyPPPoE = pcap.OpenOffline(path.Join(filePathOnlyPPPoE, fileNameOnlyPPPoE))
-		errBPFPPPoE = handleOnlyPPPoE.SetBPFFilter("(pppoes && ip) && host 77.88.21.119")
-
-		wfOnlyPPPoE, errwfOnlyPPPoE = os.OpenFile("/Users/user/pcap_test_files/pcapinfoFileOnlyPPPoE.pcap", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-		nwfOnlyPPPoE = pcapgo.NewWriter(wfOnlyPPPoE)
-		if err := nwfOnlyPPPoE.WriteFileHeader(1600, layers.LinkTypeEthernet); err != nil {
-			fmt.Println(err)
-		}
-
 	})
 
-	var _ = AfterSuite(func() {
-		file.Close()
-		fd.Close()
-		handleOnlyIP.Close()
-		wfOnlyIP.Close()
-		handleOnlyPPPoE.Close()
-		wfOnlyPPPoE.Close()
-	})
-
-	Context("Тест №1. Открытие или создание файлов", func() {
-		It("При открытии файла, для выполнения его дальнейшего декодирования, ошибок быть недолжно", func() {
-			Expect(errOpenFile).ShouldNot(HaveOccurred())
-		})
-
-		It("При открытии файла, для записи информации о результатах декодирования, ошибок быть недолжно", func() {
-			Expect(errFd).ShouldNot(HaveOccurred())
-		})
-
-		It("При открытии файла содержащего только IP, ошибок быть недолжно", func() {
-			Expect(errOnlyIP).ShouldNot(HaveOccurred())
-		})
-
-		It("При формировании BPF для поиска только по IP, ошибок быть недолжно", func() {
-			Expect(errBPFIP).ShouldNot(HaveOccurred())
-		})
-
-		It("При создании pcap файла в который выполняется запись отфильтрованных, только по IP данных, ошибки быть не должно", func() {
-			Expect(errwfOnlyIP).ShouldNot(HaveOccurred())
-		})
-
-		It("При открытии файла содержащего только PPPoE ошибок быть недолжно", func() {
-			Expect(errOnlyPPPoE).ShouldNot(HaveOccurred())
-		})
-
-		It("При формировании BPF для поиска только по PPPoE, ошибок быть недолжно", func() {
-			Expect(errBPFPPPoE).ShouldNot(HaveOccurred())
-		})
-
-		It("При создании pcap файла в который выполняется запись отфильтрованных, только по PPPoE данных, ошибки быть не должно", func() {
-			Expect(errwfOnlyPPPoE).ShouldNot(HaveOccurred())
-		})
-	})
-
-	Context("Тест №2. Читаем и декодируем файл сетевого трафика содержащий только ip.", func() {
-		It("При чтении файла не должно быть ошибок", func() {
-			foip := path.Join(filePathOnlyIP, fileNameOnlyIP)
-
-			fmt.Printf("Read file: '%v'\n", foip)
-
-			var err error
-
-			r, err := pcapgo.NewReader(file)
-			Expect(err).ShouldNot(HaveOccurred())
-
-			//data, ci, err := r.ReadPacketData()
-			//Expect(err).ShouldNot(HaveOccurred())
-
-			writer := bufio.NewWriter(fd)
-			defer func() {
-				if err == nil {
-					err = writer.Flush()
-				}
-			}()
-
-			_, err = writer.WriteString(fmt.Sprintf("Decoding file name: %v\n", foip))
-			Expect(err).ShouldNot(HaveOccurred())
-
-			/*			fmt.Println("+++++++++++++")
-						fmt.Println(r.LinkType())
-						fmt.Println(r.String())
-						fmt.Printf("--- Info file name: '%v' ---\npacket size: %v\npacket timestamp: %v\n", fileName, ci.CaptureLength, ci.Timestamp)
-						fmt.Printf("snaplen: %v\n", r.Snaplen())
-						fmt.Println("+++++++++++++")*/
-
-			var eth layers.Ethernet
-			var ip4 layers.IPv4
-			var ip6 layers.IPv6
-			var tcp layers.TCP
-			var udp layers.UDP
-			var dns layers.DNS
-			var ntp layers.NTP
-			var tls layers.TLS
-			decoded := []gopacket.LayerType{}
-			parser := gopacket.NewDecodingLayerParser(layers.LayerTypeEthernet, &eth, &ip4, &ip6, &tcp, &udp, &dns, &ntp, &tls)
-
-			/*listForEach := func(list []interface{}) string {
-				var result string
-
-				for _, l := range list {
-					switch element := l.(type) {
-					case layers.DNSQuestion:
-						result += fmt.Sprintf(" %v ", element.Name)
-					case layers.DNSResourceRecord:
-						result += fmt.Sprintf(" %v ", element.Name)
-					}
-				}
-
-				return result
-			}*/
-
-			boolToInt8 := func(v bool) int8 {
-				if v {
-					return 1
-				}
-				return 0
-			}
-
-			for {
-				data, ci, err := r.ReadPacketData()
-				if err != nil {
-					if err == io.EOF {
-						break
-					}
-				}
-
-				_, err = writer.WriteString(fmt.Sprintf("timestamp: %v,reading packets length: %v\n", ci.Timestamp, ci.CaptureLength))
-				err = parser.DecodeLayers(data, &decoded)
-				for _, layerType := range decoded {
-					//fmt.Println(layerType)
-
-					switch layerType {
-					case layers.LayerTypeIPv6:
-						_, err = writer.WriteString(fmt.Sprintf("    IP6 src:'%v', dst:'%v'\n", ip6.SrcIP, ip6.DstIP))
-					case layers.LayerTypeIPv4:
-						_, err = writer.WriteString(fmt.Sprintf("    IP4 src:'%v', dst:'%v'\n", ip4.SrcIP, ip4.DstIP))
-					case layers.LayerTypeTCP:
-						_, err = writer.WriteString(fmt.Sprintf("    TCP src port:'%v', dst port:'%v'\n", tcp.SrcPort, tcp.DstPort))
-
-						fin := boolToInt8(tcp.FIN)
-						syn := boolToInt8(tcp.SYN)
-						rst := boolToInt8(tcp.RST)
-						psh := boolToInt8(tcp.PSH)
-						ack := boolToInt8(tcp.ACK)
-						urg := boolToInt8(tcp.URG)
-
-						_, err = writer.WriteString(fmt.Sprintf("    	Flags	(FIN:'%v' SYN:'%v' RST:'%v' PSH:'%v' ACK:'%v' URG:'%v')\n", fin, syn, rst, psh, ack, urg))
-						if len(tcp.Payload) != 0 {
-							reader := bufio.NewReader(bytes.NewReader(tcp.Payload))
-
-							httpReq, err := http.ReadRequest(reader)
-							if err == nil {
-								proto := httpReq.Proto
-								method := httpReq.Method
-								//url := httpReq.URL //содержит целый тип, не только значение httpReq.RequestURI но и методы для парсинга запроса
-								host := httpReq.Host
-								reqURI := httpReq.RequestURI
-								userAgent := httpReq.Header.Get("User-Agent")
-								//_, err = writer.WriteString(fmt.Sprintf("%v\n", httpReq.Header))
-								_, err = writer.WriteString(fmt.Sprintf("    %v %v %v\n	Host:%v\n	User-Agent:%v\n", proto, method, reqURI, host, userAgent))
-							}
-
-							httpRes, err := http.ReadResponse(reader, httpReq)
-							if err == nil {
-								_, err = writer.WriteString(fmt.Sprintf("    StatusCode:%v\n", httpRes.Status))
-							}
-						}
-					case layers.LayerTypeUDP:
-						_, err = writer.WriteString(fmt.Sprintf("    UDP src port:'%v', dst port:'%v'\n", udp.SrcPort, udp.DstPort))
-					case layers.LayerTypeDNS:
-						var resultDNSQuestions, resultDNSAnswers string
-
-						for _, e := range dns.Questions {
-							resultDNSQuestions += string(e.Name)
-						}
-
-						for _, e := range dns.Answers {
-							resultDNSAnswers += fmt.Sprintf("%v (%v), %v\n", string(e.Name), e.IP, e.CNAME)
-						}
-
-						_, err = writer.WriteString(fmt.Sprintf("    Questions:'%v', Answers:'%v'\n", resultDNSQuestions, resultDNSAnswers))
-						//						_, err = writer.WriteString(fmt.Sprintf("    Questions:'%v', Answers:'%v'\n", dns.Questions, dns.Answers))
-					case layers.LayerTypeNTP:
-						_, err = writer.WriteString(fmt.Sprintf("    Version:'%v'\n", ntp.Version))
-					case layers.LayerTypeTLS:
-						_, err = writer.WriteString(fmt.Sprintf("    %v\n", tls.Handshake))
-
-					}
-				}
-			}
+	Context("Тест №1. Читаем и декодируем файл сетевого трафика содержащий только ip.", func() {
+		It("При выполнении декодирования файла сет. трафика ошибок быть не должно", func() {
+			err := networkTrafficDecoder(networkTrafficFileSettingsType{
+				filePathIn:  "/Users/user/pcap_test_files/ip",
+				fileNameIn:  "1616398942_2021_03_22____10_42_22_21.tdp",
+				filePathOut: "/Users/user/pcap_test_files",
+				fileNameOut: "pcapinfoFileOnlyIP.txt",
+			})
 
 			Expect(err).ShouldNot(HaveOccurred())
 		})
 	})
 
-	Context("Тест №3. Читаем и выполняем поиск с использованием BPF, файла, содержащего только ip", func() {
-		It("При записи результатов фильтрации в файл, ошибок быть не должно", func() {
-			packetSource := gopacket.NewPacketSource(handleOnlyIP, handleOnlyIP.LinkType())
-			for packet := range packetSource.Packets() {
-				if err := nwfOnlyIP.WritePacket(gopacket.CaptureInfo{
-					Timestamp:      packet.Metadata().Timestamp,
-					CaptureLength:  packet.Metadata().CaptureLength,
-					Length:         packet.Metadata().Length,
-					InterfaceIndex: packet.Metadata().InterfaceIndex,
-					AncillaryData:  packet.Metadata().AncillaryData,
-				}, packet.Data()); err != nil {
-					fmt.Println(err)
-				}
-			}
+	/*
+		   For filteration only VLAN
+		   				filePathIn: "/Users/user/pcap_test_files/only_vlan",
+		   				fileNameIn: "1616670006_2021_03_25____14_00_06_23.tdp",
+		   				"(( (host 46.42.4.164))) || (vlan && (( (host 46.42.4.164))))"
 
-			Expect(true).Should(BeTrue())
+						   Для такого шаблона поиск по IP или же VLAN работает полностью
+	*/
+
+	Context("Тест №2. Читаем и выполняем поиск с использованием BPF, файла, содержащего только ip", func() {
+		It("При выполнении фильтрации файла содержащего только IP, ошибок быть не должно", func() {
+			err := networkTrafficFilter(networkTrafficFileSettingsType{
+				filePathIn:  "/Users/user/pcap_test_files/ip",
+				fileNameIn:  "1616398942_2021_03_22____10_42_22_21.tdp",
+				filePathOut: "/Users/user/pcap_test_files",
+				fileNameOut: "pcapinfoFileOnlyIP.pcap",
+			}, "(( (host 77.241.31.37))) || (vlan && (( (host 77.241.31.37))))")
+
+			Expect(err).ShouldNot(HaveOccurred())
 		})
 	})
 
-	Context("Тест №4. Читаем и выполняем поиск с использованием BPF, файла, содержащего только PPPoE", func() {
-		It("При записи результатов фильтрации в файл, ошибок быть не должно", func() {
-			packetSource := gopacket.NewPacketSource(handleOnlyPPPoE, handleOnlyPPPoE.LinkType())
-			for packet := range packetSource.Packets() {
-				if err := nwfOnlyPPPoE.WritePacket(gopacket.CaptureInfo{
-					Timestamp:      packet.Metadata().Timestamp,
-					CaptureLength:  packet.Metadata().CaptureLength,
-					Length:         packet.Metadata().Length,
-					InterfaceIndex: packet.Metadata().InterfaceIndex,
-					AncillaryData:  packet.Metadata().AncillaryData,
-				}, packet.Data()); err != nil {
-					fmt.Println(err)
-				}
-			}
+	/*
+		Шаблон "(pppoes && ip) && (( (host 77.88.21.119))) || (vlan && (( (host 77.88.21.119))))"" handle.SetBPFFilter
+		компилировать отказывается, однако шаблон "(pppoes && ip) && (( (host 77.88.21.119)))" успешно проходит
+		компиляцию и с ним находятся сетевые пакеты
+	*/
+	Context("Тест №3. Читаем и выполняем поиск с использованием BPF, файла, содержащего только PPPoE", func() {
+		It("При выполнении фильтрации файла содержащего только PPPoE, ошибок быть не должно", func() {
+			err := networkTrafficFilter(networkTrafficFileSettingsType{
+				filePathIn:  "/Users/user/pcap_test_files/pppoe",
+				fileNameIn:  "1616149545_2021_03_19____13_25_45_3596.tdp",
+				filePathOut: "/Users/user/pcap_test_files",
+				fileNameOut: "pcapinfoFileOnlyPPPoE.pcap",
+			}, "(pppoes && ip) && (( (host 77.88.21.119)))")
 
-			Expect(true).Should(BeTrue())
+			Expect(err).ShouldNot(HaveOccurred())
 		})
 	})
+
+	/*
+	   '(pppoes && ip) && (( (host 87.250.250.192))) || (vlan && (( (host 87.250.250.192))))'
+	   '(pppoes && vlan && ip) && (( (host 87.250.250.192))) || (vlan && (( (host 87.250.250.192))))'
+	*/
 
 	/*Context("Тест №2. Декодируем pcap файл.", func() {
 		It("При декодировании файла по сетевым протоколоам не должно быть ошибок", func() {
@@ -337,3 +121,201 @@ var _ = Describe("PacketDecoder", func() {
 						fmt.Println(packet)
 					}
 */
+
+//networkTrafficFileSettingsType
+// filePathIn - директория в которой находится файл подлежащий декодированию
+// fileNameIn - имя файла подлежащего декодированию
+// filePathOut - директория в которою сохраняются результаты декодированию
+// fileNameOut - имя файла в который сохраняются результаты дикодирования
+type networkTrafficFileSettingsType struct {
+	filePathIn  string
+	fileNameIn  string
+	filePathOut string
+	fileNameOut string
+}
+
+//networkTrafficDecoder декодировщик сетевого трафика
+func networkTrafficDecoder(ntfs networkTrafficFileSettingsType) error {
+	inputFile := path.Join(ntfs.filePathIn, ntfs.fileNameIn)
+
+	fmt.Printf("Read file: '%v'\n", inputFile)
+
+	// для файла по которому выполняется декодирование пакетов
+	file, err := os.Open(inputFile)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// для файла в который выполняется запись информации полученной в результате декодирования
+	fd, err := os.OpenFile(path.Join(ntfs.filePathOut, ntfs.fileNameOut), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		return err
+	}
+	defer fd.Close()
+
+	r, err := pcapgo.NewReader(file)
+	if err != nil {
+		return err
+	}
+
+	writer := bufio.NewWriter(fd)
+	defer func() {
+		if err == nil {
+			err = writer.Flush()
+		}
+	}()
+
+	_, writeErr := writer.WriteString(fmt.Sprintf("Decoding file name: %v\n", inputFile))
+	if writeErr != nil {
+		return writeErr
+	}
+
+	var (
+		eth layers.Ethernet
+		ip4 layers.IPv4
+		ip6 layers.IPv6
+		tcp layers.TCP
+		udp layers.UDP
+		dns layers.DNS
+		ntp layers.NTP
+		tls layers.TLS
+	)
+	decoded := []gopacket.LayerType{}
+	parser := gopacket.NewDecodingLayerParser(layers.LayerTypeEthernet, &eth, &ip4, &ip6, &tcp, &udp, &dns, &ntp, &tls)
+
+	boolToInt8 := func(v bool) int8 {
+		if v {
+			return 1
+		}
+		return 0
+	}
+
+	for {
+		data, ci, e := r.ReadPacketData()
+		if e != nil {
+			if e == io.EOF {
+				break
+			}
+		}
+
+		_, writeErr := writer.WriteString(fmt.Sprintf("timestamp: %v,reading packets length: %v\n", ci.Timestamp, ci.CaptureLength))
+		if writeErr != nil {
+			break
+		}
+
+		e = parser.DecodeLayers(data, &decoded)
+		if e != nil {
+			continue
+		}
+
+		for _, layerType := range decoded {
+			switch layerType {
+			case layers.LayerTypeIPv6:
+				_, writeErr = writer.WriteString(fmt.Sprintf("    IP6 src:'%v', dst:'%v'\n", ip6.SrcIP, ip6.DstIP))
+			case layers.LayerTypeIPv4:
+				_, writeErr = writer.WriteString(fmt.Sprintf("    IP4 src:'%v', dst:'%v'\n", ip4.SrcIP, ip4.DstIP))
+			case layers.LayerTypeTCP:
+				_, writeErr = writer.WriteString(fmt.Sprintf("    TCP src port:'%v', dst port:'%v'\n", tcp.SrcPort, tcp.DstPort))
+
+				fin := boolToInt8(tcp.FIN)
+				syn := boolToInt8(tcp.SYN)
+				rst := boolToInt8(tcp.RST)
+				psh := boolToInt8(tcp.PSH)
+				ack := boolToInt8(tcp.ACK)
+				urg := boolToInt8(tcp.URG)
+
+				_, writeErr = writer.WriteString(fmt.Sprintf("    	Flags	(FIN:'%v' SYN:'%v' RST:'%v' PSH:'%v' ACK:'%v' URG:'%v')\n", fin, syn, rst, psh, ack, urg))
+				if len(tcp.Payload) != 0 {
+					reader := bufio.NewReader(bytes.NewReader(tcp.Payload))
+
+					httpReq, errHTTP := http.ReadRequest(reader)
+					if errHTTP == nil {
+						proto := httpReq.Proto
+						method := httpReq.Method
+						//url := httpReq.URL //содержит целый тип, не только значение httpReq.RequestURI но и методы для парсинга запроса
+						host := httpReq.Host
+						reqURI := httpReq.RequestURI
+						userAgent := httpReq.Header.Get("User-Agent")
+						//_, writeErr = writer.WriteString(fmt.Sprintf("%v\n", httpReq.Header))
+						_, writeErr = writer.WriteString(fmt.Sprintf("    %v %v %v\n	Host:%v\n	User-Agent:%v\n", proto, method, reqURI, host, userAgent))
+					}
+
+					httpRes, errHTTP := http.ReadResponse(reader, httpReq)
+					if errHTTP == nil {
+						_, writeErr = writer.WriteString(fmt.Sprintf("    StatusCode:%v\n", httpRes.Status))
+					}
+				}
+			case layers.LayerTypeUDP:
+				_, writeErr = writer.WriteString(fmt.Sprintf("    UDP src port:'%v', dst port:'%v'\n", udp.SrcPort, udp.DstPort))
+			case layers.LayerTypeDNS:
+				var resultDNSQuestions, resultDNSAnswers string
+
+				for _, dnsQ := range dns.Questions {
+					resultDNSQuestions += string(dnsQ.Name)
+				}
+
+				for _, dnsA := range dns.Answers {
+					resultDNSAnswers += fmt.Sprintf("%v (%v), %v\n", string(dnsA.Name), dnsA.IP, dnsA.CNAME)
+				}
+
+				_, writeErr = writer.WriteString(fmt.Sprintf("    Questions:'%v', Answers:'%v'\n", resultDNSQuestions, resultDNSAnswers))
+				//						_, err = writer.WriteString(fmt.Sprintf("    Questions:'%v', Answers:'%v'\n", dns.Questions, dns.Answers))
+			case layers.LayerTypeNTP:
+				_, writeErr = writer.WriteString(fmt.Sprintf("    Version:'%v'\n", ntp.Version))
+			case layers.LayerTypeTLS:
+				_, writeErr = writer.WriteString(fmt.Sprintf("    %v\n", tls.Handshake))
+
+			}
+		}
+	}
+
+	return err
+}
+
+//networkTrafficFilter
+func networkTrafficFilter(ntfs networkTrafficFileSettingsType, pattern string) error {
+	var (
+		handle *pcap.Handle
+		wf     *os.File
+		nwf    *pcapgo.Writer
+		err    error
+	)
+
+	handle, err = pcap.OpenOffline(path.Join(ntfs.filePathIn, ntfs.fileNameIn))
+	if err != nil {
+		return err
+	}
+	defer handle.Close()
+
+	err = handle.SetBPFFilter(pattern)
+	if err != nil {
+		return err
+	}
+
+	wf, err = os.OpenFile(path.Join(ntfs.filePathOut, ntfs.fileNameOut), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		return err
+	}
+	defer wf.Close()
+
+	nwf = pcapgo.NewWriter(wf)
+	if err := nwf.WriteFileHeader(1600, layers.LinkTypeEthernet); err != nil {
+		return err
+	}
+
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	for packet := range packetSource.Packets() {
+		if err := nwf.WritePacket(gopacket.CaptureInfo{
+			Timestamp:      packet.Metadata().Timestamp,
+			CaptureLength:  packet.Metadata().CaptureLength,
+			Length:         packet.Metadata().Length,
+			InterfaceIndex: packet.Metadata().InterfaceIndex,
+			AncillaryData:  packet.Metadata().AncillaryData,
+		}, packet.Data()); err != nil {
+			break
+		}
+	}
+
+	return err
+}
